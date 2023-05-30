c892b46c9c589773e5764602b2223cef7dd1b6db
lee-orr <lee-orr@users.noreply.github.com>
2023-05-30 23:58:38 +0000

    fix wasm build script
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 cargo build --release --target wasm32-unknown-unknown
 
-wasm-bindgen --out-dir ./dist/ --target web ./target/
+wasm-bindgen --out-dir ./dist/ --target web ./target/wasm32-unknown-unknown/release/terra-and-caelus.wasm 
 
 cp index.html ./dist/index.html
 cp -R ./assets ./dist/assets
